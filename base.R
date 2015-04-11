b6864a06273be28093189418ec5719c4342de93a
Ricardo Walter Hildebrand <ricardo.walter@gmail.com>
2015-04-11 18:34:37 -0300

    plot 1

--- base.R
+++ base.R
@@ -1,29 +1,52 @@
 library(utils)
 
 readdata <- function() {
-    getCompressedDataFile()
-    decompressDataFile()
-    "aqui"
-}
-
-getCompressedDataFile <- function() {
-    url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-    zipname <- "data/household_power_consumption.zip"
-    
-    if (!file.exists(zipname)) {
-        ret <- download.file(url, zipname, quiet=T)
-        if (ret != 0) {
-            stop("Download error. Check internet connection.")
+    getCompressedDataFile <- function() {
+        url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        zipname <- "data/household_power_consumption.zip"
+        
+        if (!file.exists(zipname)) {
+            ret <- download.file(url, zipname, quiet=T)
+            if (ret != 0) {
+                stop("Download error. Check internet connection.")
+            }
         }
     }
+    
+    decompressDataFile <- function() {
+        zipname <- "data/household_power_consumption.zip"
+        dataname <- "data/household_power_consumption.txt" 
+        filenames <- c("household_power_consumption.txt")
+        
+        if (!file.exists(dataname)) {
+            unzip(zipname, filenames, exdir= "./data")
+        }
+    }
+    
+    loadAndClean <- function() {
+        dataname <- "data/household_power_consumption.txt" 
+        cachename <- "data/household_power_consumption_cleaned.rds"
+        
+        if (!file.exists(cachename)) {
+            data <- read.csv(dataname, sep = ";", na.strings = "?", dec = ".")
+            data <- transform(data, Date = as.Date(Date, format="%d/%m/%Y"))
+            data <- transform(data, Time = as.POSIXlt(as.character(paste(Date, Time, sep = " ")), format="%Y-%m-%d %H:%M:%S"))
+            data <- subset(data, Date == "2007-02-01" | Date == "2007-02-02")
+            saveRDS(data, file = cachename)
+        } else {
+            data <- readRDS(cachename)
+        }
+        
+        data
+    }
+
+    getCompressedDataFile()
+    decompressDataFile()
+    loadAndClean()
 }
 
-decompressDataFile <- function() {
-    zipname <- "data/household_power_consumption.zip"
-    dataname <- "data/household_power_consumption.txt" 
-    filenames <- c("household_power_consumption.txt")
-    
-    if (!file.exists(dataname)) {
-        unzip(zipname, filenames, exdir= "./data")
-    }
+savePlot <- function (filename, plotFunction) {
+    png(filename, width=480, height=480, res=120)
+    plotFunction()
+    dev.off()
 }
--- plot1.R
+++ plot1.R
@@ -1,6 +1,15 @@
 source('base.R')
 
 plot1 <- function() {
-    data <- readdata()
-    data
+    plot <- function() {
+        with(data, hist(
+            Global_active_power, 
+            main = "Global Active Power", 
+            xlab = "Global Active Power (kilowatts)", 
+            ylab = "Frequency", 
+            col = "red"))
+    }
+
+    savePlot("plot1.png", plot)
+    plot()
 }
